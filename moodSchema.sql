975fcc1d2ee9c2f5d79e4e875e076b2d06374561
Haneya Yunus <haneya@alertlabs.com>
2017-06-06 20:13:17 -0400

    added database script.

---
+++ moodSchema.sql
@@ -0,0 +1,11 @@
+/*
+* Created by Haneya Yunus
+*
+* This script outlines the creation of the database schema required for mood. The rest is done by the application itself.
+* Run MySQL on localhost.
+* Make sure you change the MySQL password (to your root password) in the app.py file to access it!
+*/
+
+CREATE DATABASE mood;
+USE mood;
+CREATE TABLE 'users' ('id' BIGINT NULL AUTOINCREMENT, 'username' VARCHAR(45) NULL, 'password' VARCHAR(45) NULL);
